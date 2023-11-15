3d9996f5f5050d9f4a203c90d575978618fc90cd
koa-connectline <koa@connect-line.gr>
2023-11-15 17:35:21 +0200

    fix(telescope): changed insert mode next/prev selection mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,11 +23,11 @@ return {
 		},
 		mappings = {
 			i = {
-				["<C-n>"] = actions.move_selection_next,
-				["<C-p>"] = actions.move_selection_previous,
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
 				["<C-c>"] = actions.close,
-				["<C-j>"] = actions.cycle_history_next,
-				["<C-k>"] = actions.cycle_history_prev,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
 				["<C-q>"] = function(...)
 					actions.smart_send_to_qflist(...)
 					actions.open_qflist(...)
