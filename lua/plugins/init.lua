40fe32411c363982f80c478ce733993161a33c2d
koa-connectline <koa@connect-line.gr>
2024-01-24 15:32:02 +0200

    fix(plugins): testing different db plugins + added csv.vim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -514,4 +514,37 @@ return {
         dependencies = require("plugins.dadbod").dependencies,
         config = require("plugins.dadbod").config,
     },
+    {
+        "chrisbra/csv.vim",
+        ft = "csv",
+    },
+    -- {
+    --     "kndndrj/nvim-dbee",
+    --     lazy = true,
+    --     dependencies = {
+    --         "MunifTanjim/nui.nvim",
+    --     },
+    --     build = function()
+    --         -- Install tries to automatically detect the install method.
+    --         -- if it fails, try calling it with one of these parameters:
+    --         --    "curl", "wget", "bitsadmin", "go"
+    --         require("dbee").install()
+    --     end,
+    --     config = function()
+    --         require("dbee").setup({
+    --             sources = {
+    --                 require("dbee.sources").MemorySource:new({
+    --                     -- {
+    --                     --     name = "",
+    --                     --     type = "",
+    --                     --     url = "",
+    --                     -- },
+    --                     -- ...
+    --                 }),
+    --                 -- require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
+    --                 -- require("dbee.sources").FileSource:new(vim.fn.stdpath("cache") .. "/dbee/persistence.json"),
+    --             },
+    --         })
+    --     end,
+    -- },
 }
