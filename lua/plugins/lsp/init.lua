556abccad08a9bab7e7db07b9f8121cd245cef0d
akotro <akotronakis@hotmail.com>
2024-06-06 16:15:41 +0300

    chore(plugins): updates

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -267,47 +267,6 @@ M.dependencies = {
 
     -- NOTE: LSP Language Extensions
     { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true, ft = "cs" },
-    -- {
-    --     "simrat39/rust-tools.nvim",
-    --     lazy = true,
-    --     ft = { "rust", "toml" },
-    --     opts = function()
-    --         local ok, mason_registry = pcall(require, "mason-registry")
-    --         local adapter ---@type any
-    --         if ok then
-    --             -- rust tools configuration for debugging support
-    --             local codelldb = mason_registry.get_package("codelldb")
-    --             local extension_path = codelldb:get_install_path() .. "/extension/"
-    --             local codelldb_path = extension_path .. "adapter/codelldb"
-    --             local liblldb_path = ""
-    --             if vim.loop.os_uname().sysname:find("Windows") then
-    --                 liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-    --             elseif vim.fn.has("mac") == 1 then
-    --                 liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-    --             else
-    --                 liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-    --             end
-    --             adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-    --         end
-    --         return {
-    --             dap = {
-    --                 adapter = adapter,
-    --             },
-    --             tools = {
-    --                 on_initialized = function()
-    --                     vim.cmd([[
-    --                         augroup RustLSP
-    --                             " autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-    --                             autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-    --                             autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-    --                         augroup END
-    -- 		]])
-    --                 end,
-    --             },
-    --         }
-    --     end,
-    --     config = function() end,
-    -- },
     {
         "mrcjkb/rustaceanvim",
         version = "^4",
@@ -403,8 +362,16 @@ M.opts = {
             -- keys = {},
             settings = {
                 Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
+                    },
                     workspace = {
                         checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                        },
                     },
                     completion = {
                         callSnippet = "Replace",
@@ -555,8 +522,6 @@ M.opts = {
             return true
         end,
         rust_analyzer = function(_, opts)
-            -- local rust_tools_opts = require("config.functions").plugin.opts("rust-tools.nvim")
-            -- require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
             return true
         end,
         clangd = function(_, opts)
