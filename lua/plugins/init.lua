c06e2b22fe6af8d11d8819d687294efb1dfe2671
akotro <akotronakis@hotmail.com>
2024-09-15 18:00:25 +0300

    feat(plugins): added bufjump.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -267,6 +267,41 @@ return {
             require("kitty-scrollback").setup()
         end,
     },
+    {
+        "kwkarlwang/bufjump.nvim",
+        lazy = true,
+        config = {},
+        keys = {
+            {
+                "<C-n>",
+                function()
+                    require("bufjump").forward()
+                end,
+                "Jumplist forward",
+            },
+            {
+                "<C-p>",
+                function()
+                    require("bufjump").backward()
+                end,
+                "Jumplist backward",
+            },
+            {
+                "<M-i>",
+                function()
+                    require("bufjump").forward_same_buf()
+                end,
+                "Jumplist forward same buffer",
+            },
+            {
+                "<M-o>",
+                function()
+                    require("bufjump").backward_same_buf()
+                end,
+                "Jumplist backward same buffer",
+            },
+        },
+    },
     -- NOTE: Git
     { "tpope/vim-fugitive", event = "BufRead" },
     {
