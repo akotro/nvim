607d2098d1c30a84d0ef6a38da77a163087a995e
akotro <akotronakis@hotmail.com>
2024-11-01 12:06:59 +0000

    chore(plugins): updates
    commented out some indent_blankline config, waiting for https://github.com/lukas-reineke/indent-blankline.nvim/issues/936

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -3,14 +3,15 @@ local M = {}
 local icons = require("config.ui").icons
 
 M.opts = {
-    indent = {
-        char = icons.ui.LineLeft,
-        tab_char = icons.ui.LineLeft,
-    },
-    scope = {
-        enabled = false,
-        char = icons.ui.LineLeft,
-    },
+    -- NOTE: Commented due to https://github.com/lukas-reineke/indent-blankline.nvim/issues/936
+    -- indent = {
+    --     char = icons.ui.LineLeft,
+    --     tab_char = icons.ui.LineLeft,
+    -- },
+    -- scope = {
+    --     enabled = false,
+    --     char = icons.ui.LineLeft,
+    -- },
     exclude = {
         buftypes = { "terminal", "nofile" },
         filetypes = {
