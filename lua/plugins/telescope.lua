6ab67e02c8d0b708573b5dfe7051dc5fa5669224
akotro <mail@akotro.dev>
2024-12-22 19:08:25 +0200

    fix(telescope): adjust picker mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -77,11 +77,9 @@ function M.opts()
             },
             mappings = {
                 i = {
-                    ["<C-n>"] = actions.cycle_history_next,
-                    ["<C-p>"] = actions.cycle_history_prev,
                     ["<C-c>"] = actions.close,
-                    ["<C-j>"] = actions.move_selection_next,
-                    ["<C-k>"] = actions.move_selection_previous,
+                    ["<C-j>"] = actions.cycle_history_next,
+                    ["<C-k>"] = actions.cycle_history_prev,
                     ["<C-q>"] = function(...)
                         actions.smart_send_to_qflist(...)
                         actions.open_qflist(...)
