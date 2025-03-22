b1598e5097af73873bba913e273dd3f24817918d
akotro <mail@akotro.dev>
2025-03-22 20:14:26 +0000

    feat(gitsigns): added git reset keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -67,13 +67,6 @@ M.keys = {
         end,
         desc = "Git Prev Hunk",
     },
-    -- {
-    --     "<leader>gp",
-    --     function()
-    --         require("gitsigns").preview_hunk()
-    --     end,
-    --     desc = "Preview Hunk",
-    -- },
     {
         "<leader>gB",
         function()
@@ -81,43 +74,22 @@ M.keys = {
         end,
         desc = "Git Blame Line",
     },
-    -- {
-    --     "<leader>gl",
-    --     function()
-    --         require("gitsigns").blame_line()
-    --     end,
-    --     desc = "Blame Line",
-    -- },
-    -- {
-    --     "<leader>gr",
-    --     function()
-    --         require("gitsigns").reset_hunk()
-    --     end,
-    --     desc = "Reset Hunk",
-    --     nowait = true,
-    --     remap = false,
-    -- },
-    -- {
-    --     "<leader>gR",
-    --     function()
-    --         require("gitsigns").reset_buffer()
-    --     end,
-    --     desc = "Reset Buffer",
-    -- },
-    -- {
-    --     "<leader>gs",
-    --     function()
-    --         require("gitsigns").stage_hunk()
-    --     end,
-    --     desc = "Stage Hunk",
-    -- },
-    -- {
-    --     "<leader>gu",
-    --     function()
-    --         require("gitsigns").undo_stage_hunk()
-    --     end,
-    --     desc = "Undo Stage Hunk",
-    -- },
+    {
+        "<leader>gr",
+        function()
+            require("gitsigns").reset_hunk()
+        end,
+        desc = "Reset Hunk",
+        nowait = true,
+        remap = false,
+    },
+    {
+        "<leader>gR",
+        function()
+            require("gitsigns").reset_buffer()
+        end,
+        desc = "Reset Buffer",
+    },
 }
 
 return M
