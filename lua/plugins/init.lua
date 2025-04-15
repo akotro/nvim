e9b7f8fea545c163102b26d96dd9ff59ccfa9e34
akotro <mail@akotro.dev>
2025-04-15 19:03:02 +0300

    feat(plugins): added auto-cmdheight.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -234,6 +234,22 @@ return {
             },
         },
     },
+    {
+        -- NOTE: Until this features gets implemented in core
+        "jake-stewart/auto-cmdheight.nvim",
+        lazy = false,
+        opts = {
+            -- max cmdheight before displaying hit enter prompt.
+            max_lines = 5,
+            -- number of seconds until the cmdheight can restore.
+            duration = 2,
+            -- whether key press is required to restore cmdheight.
+            remove_on_key = true,
+            -- always clear the cmdline after duration and key press.
+            -- by default it will only happen when cmdheight changed.
+            clear_always = false,
+        },
+    },
     -- NOTE: Git
     {
         "tpope/vim-fugitive",
